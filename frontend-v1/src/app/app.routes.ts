a6dde36a3a9e5360d3a7fa1f18f4e35ad9713c64
Arif Khan <arif.bannehasan@gmail.com>
2025-03-01 22:48:10 +0530

    Add initial styles, environment configurations, and models for GIC Cinemas application

---
+++ frontend-v1/src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { MovieSetupComponent } from './components/movie-setup/movie-setup.component';
+import { BookTicketsComponent } from './components/book-tickets/book-tickets.component';
+import { CheckBookingsComponent } from './components/check-bookings/check-bookings.component';
+
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'setup', component: MovieSetupComponent },
+  { path: 'book', component: BookTicketsComponent },
+  { path: 'bookings', component: CheckBookingsComponent },
+  { path: '**', redirectTo: '' }
+];
